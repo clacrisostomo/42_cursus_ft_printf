c13f4b96b6ddf22803e7a6d55245846adff2e749
clacrisostomo <clacrisostomo@gmail.com>
2021-04-15 20:53:38 -0300

    updating

--- print_cs_per.c
+++ print_cs_per.c
@@ -6,7 +6,7 @@
 /*   By: csantos- <csantos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 21:10:05 by csantos-          #+#    #+#             */
-/*   Updated: 2021/04/15 20:51:29 by csantos-         ###   ########.fr       */
+/*   Updated: 2021/04/15 20:53:28 by csantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ void			print_s(t_flags *flags, char *s)
 
 	if (!s)
 		s = "(null)";
-	if ((char *)s == "")
+	if (s == '\0')
 		flags->len--;
 	size = (int)ft_strlen(s);
 	//printf("size = %i\n", size);
