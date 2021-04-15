2e64b381ae2f6c3605d8b45b327f8cb228d94e3d
clacrisostomo <clacrisostomo@gmail.com>
2021-04-15 00:08:34 -0300

    updating

--- utils_a.c
+++ utils_a.c
@@ -6,7 +6,7 @@
 /*   By: csantos- <csantos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 21:11:12 by csantos-          #+#    #+#             */
-/*   Updated: 2021/04/15 00:07:42 by csantos-         ###   ########.fr       */
+/*   Updated: 2021/04/15 00:08:31 by csantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,11 +127,11 @@ char		*hextoa(t_flags *flags, unsigned long long nb)
 	{
 		temp = temp % 16;
 		if ((flags->type == 'x' || flags->type == 'p') && temp >= 10)
-			str[count] = temp + 87;
+			str[count - 1] = temp + 87;
 		else if (flags->type == 'X' && temp >= 10)
-			str[count] = temp + 55;
+			str[count - 1] = temp + 55;
 		else
-			str[count] = temp + 48;
+			str[count - 1] = temp + 48;
 		temp = temp / 16;
 		count--;
 	}
